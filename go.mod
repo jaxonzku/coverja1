e7cdf384ec0d5d2dceac6ff1409f10d1fb7a39a8
jaxonzku <jacksonj0007@gmail.com>
2021-07-24 18:56:39 +0530

    ui
    ui

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module example.com/m
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/lib/pq v1.10.2 // indirect
--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
@@ -28,6 +32,22 @@ type Luser struct {
 	Password string `json:"password"`
 }
 
+func CreateToken(email string) (string, error) {
+	var err error
+	//Creating Access Token
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userid
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
@@ -38,9 +58,12 @@ func CheckPasswordHash(password, hash string) bool {
 	return err == nil
 }
 
+func createblog(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func signUp(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("signup hit")
-	//fmt.Fprintf(w, "signup hit")
 	db := dbconnect()
 
 	var p User
@@ -54,10 +77,10 @@ func signUp(w http.ResponseWriter, r *http.Request) {
 	password = p.Password
 	hash, _ := HashPassword(password)
 
-	sqlStatement := `INSERT INTO emp (name, phone,email,password) VALUES ($1,$2,$3,$4)`
+	sqlStatement := `INSERT INTO users (name, phone,email,password) VALUES ($1,$2,$3,$4)`
 	_, err = db.Exec(sqlStatement, p.Name, p.Phone, p.Email, hash)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusOK)
 		fmt.Println("error in here")
 		fmt.Fprintf(w, "email is  already there")
 		//panic(err)
@@ -78,7 +101,7 @@ func signIn(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	result := db.QueryRow("select password from emp where email=$1", l.Email)
+	result := db.QueryRow("select password from users where email=$1", l.Email)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -104,6 +127,7 @@ func signIn(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("wrong pass")
 	} else {
 		fmt.Fprintf(w, "password matches")
+		w.WriteHeader(http.StatusAccepted)
 
 	}
 
@@ -112,13 +136,24 @@ func signIn(w http.ResponseWriter, r *http.Request) {
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("in homepage")
 	fmt.Fprintf(w, "homepage hit")
-}
+	type Welcome struct {
+		Name string
+		Time string
+	}
 
+	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
+	templates := template.Must(template.ParseFiles("templates/home.html"))
+
+	if err := templates.ExecuteTemplate(w, "home.html", welcome); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage).Methods("GET")
 	myRouter.HandleFunc("/api/signUp", signUp).Methods("POST")
 	myRouter.HandleFunc("/api/signIn", signIn).Methods("POST")
+	myRouter.HandleFunc("/api/createblog", createblog).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
@@ -135,12 +170,13 @@ func dbconnect() *sql.DB {
 	}
 
 	fmt.Printf("\nSuccessfully connected to database!\n")
-	//fmt.Println(db)
+
 	return db
 
 }
 
 func main() {
+	fmt.Printf("running. . . . . . ")
 
 	handleRequests()
 
